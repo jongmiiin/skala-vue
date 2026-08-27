8c603dd8343b6477b8f8b406f861d39016b5d881
jongmin <pjm0409@naver.com>
2026-08-27 20:40:06 +0900

    feat: Library 실습 Element Plus 예제 구현

--- src/views/LibraryView.vue
+++ src/views/LibraryView.vue
@@ -2,7 +2,7 @@
 import StoreCounter from '@/components/practices/library/StoreCounter.vue'
 import AxiosWeather from '@/components/practices/library/AxiosWeather.vue'
 import AxiosJson from '@/components/practices/library/AxiosJson.vue'
-// import ElementPlus from '@/components/practices/library/ElementPlus.vue'
+import ElementPlus from '@/components/practices/library/ElementPlus.vue'
 // import EcmaScript from '@/components/practices/library/EcmaScript.vue'
 </script>
 
@@ -17,7 +17,7 @@ import AxiosJson from '@/components/practices/library/AxiosJson.vue'
     <AxiosJson />
     <h1>3. Element Plus</h1>
     <hr />
-    <!-- <ElementPlus /> -->
+    <ElementPlus />
     <h1>4. Modern JavaScript</h1>
     <hr />
     <!-- <EcmaScript /> -->
